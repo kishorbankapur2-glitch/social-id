6939e9c81a64eea74fe89ae6249a67e91629a178
kishorbankapur2-glitch <kishorbankapur2@gmail.com>
2026-03-02 12:58:16 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'secret-lab-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './photo1.png',
+  './audio.mp3',
+  './audio2.mp3'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
